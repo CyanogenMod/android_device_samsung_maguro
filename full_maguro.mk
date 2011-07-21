1eb33d1b0572550873894f39497e06127d9a580f
Jean-Baptiste Queru <jbq@google.com>
2011-07-20 18:29:19 -0700

    Add Camera back
    Change-Id: I0dab49b5a88d4e8a5bdcedfb07798f3ce363891d

--- full_maguro.mk
+++ full_maguro.mk
@@ -21,6 +21,10 @@
 # lines, full and maguro, hence its name.
 #
 
+# Camera
+PRODUCT_PACKAGES := \
+    Camera
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
