4c41b0dfb7bbbd36c110fa60df000b0d4b26378f
Jean-Baptiste Queru <jbq@google.com>
2011-11-23 12:43:41 -0800

    Generate factory images for itl41f
    Change-Id: If63d67c88bd5039de36722a8f00dc15cf404c196

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -14,13 +14,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# 223971 = ITL41D
+# 228551 = ITL41F
+
+BUILD=228551
+VERSION=itl41f
+
 rm -rf tmp
-mkdir -p tmp/yakju-itl41d
-unzip -d tmp signed-yakju-target_files-223971.zip RADIO/radio.img RADIO/bootloader.img
-cp signed-yakju-img-223971.zip tmp/yakju-itl41d/image-yakju-itl41d.zip
-cp tmp/RADIO/bootloader.img tmp/yakju-itl41d/bootloader-maguro-primekj10.img
-cp tmp/RADIO/radio.img tmp/yakju-itl41d/radio-maguro-i9250xxkk1.img
-cp flash-all.sh tmp/yakju-itl41d
-(cd tmp ; tar zcvf ../yakju-itl41d-factory.tgz yakju-itl41d)
-mv yakju-itl41d-factory.tgz yakju-itl41d-factory-$(sha1sum < yakju-itl41d-factory.tgz | cut -b -8).tgz
+mkdir -p tmp/yakju-$VERSION
+unzip -d tmp signed-yakju-target_files-$BUILD.zip RADIO/radio.img RADIO/bootloader.img
+cp signed-yakju-img-$BUILD.zip tmp/yakju-$VERSION/image-yakju-$VERSION.zip
+cp tmp/RADIO/bootloader.img tmp/yakju-$VERSION/bootloader-maguro-primekj10.img
+cp tmp/RADIO/radio.img tmp/yakju-$VERSION/radio-maguro-i9250xxkk1.img
+cp flash-all.sh tmp/yakju-$VERSION
+(cd tmp ; tar zcvf ../yakju-$VERSION-factory.tgz yakju-$VERSION)
+mv yakju-$VERSION-factory.tgz yakju-$VERSION-factory-$(sha1sum < yakju-$VERSION-factory.tgz | cut -b -8).tgz
 rm -rf tmp
