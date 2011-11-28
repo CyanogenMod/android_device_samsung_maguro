3622693b986e3e039284ffa677e1a458c647fa1c
BumbleBee <bigbeeshane@gmail.com>
2011-11-28 16:58:26 +0000

    update : cm.mk
    inline with new include structure

--- cm.mk
+++ cm.mk
@@ -1,21 +1,18 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := GN
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/device.mk)
 
-PRODUCT_NAME := cm_maguro
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := GN
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cm/config/common_versions.mk
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
+PRODUCT_NAME := cm_maguro
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_ID=ITL41D BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT="google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys" PRVIATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys"
