1c361a65257861124e48b08ecf5f945ee68f4f40
BumbleBee <bigbeeshane@gmail.com>
2011-12-01 20:34:25 +0000

    update : cm.mk
    - inherit full_maguro.mk instead of device.mk (this also includes full_base ect.)
    - override PRODUCT_BRAND and PRODUCT_MODEL set in full_maguro.mk
    
    Change-Id: Ia50ded96bc0cb0e92f8c1f205ea4ac39d3c26392

--- cm.mk
+++ cm.mk
@@ -8,11 +8,13 @@ PRODUCT_RELEASE_NAME := GN
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/maguro/device.mk)
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
 PRODUCT_NAME := cm_maguro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_ID=ITL41D BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT="google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys" PRVIATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys"
