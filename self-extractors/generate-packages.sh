db4b08f94524f879ec943666e58c2d497b40b745
Jean-Baptiste Queru <jbq@google.com>
2011-12-16 08:36:13 -0800

    Self-extractors for IML74K
    Change-Id: I8f144fc8e72fd3bedab4ca7c5399ab12b934de01

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -21,8 +21,9 @@
 # 237867 = IML74B
 # 238432 = IML74E
 # 238649 = IML74G
-ZIP=yakju-ota-238649.zip
-BUILD=iml74g
+# 239410 = IML74K
+ZIP=yakju-ota-239410.zip
+BUILD=iml74k
 ROOTDEVICE=maguro
 DEVICE=maguro
 MANUFACTURER=samsung
