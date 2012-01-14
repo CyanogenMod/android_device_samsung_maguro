ae7ed458d29ae16acc9f11bf9bf520e42f31a1ed
fitsnugly <rickfitsnugly@gmail.com>
2012-01-13 21:01:10 -0500

    maguro: inherit camera effects videos
    Change-Id: Ie7927d6c0c0b42db1e482a60c4f015fb97a147e8

--- cm.mk
+++ cm.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
+# Inherit camera effects videos
+$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
 PRODUCT_NAME := cm_maguro
