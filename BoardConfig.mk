5e7e03f6f18e9512d07167286a460d18f9127fc6
Doug Zongker <dougz@android.com>
2012-01-19 10:44:53 -0800

    split board-info.txt into AOSP and non-AOSP versions
    Change-Id: Idbfb10b2dd6540f027f2ab44fef8f056998eeadf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,10 @@
 # Use the non-open-source part, if present
 -include vendor/samsung/maguro/BoardConfigVendor.mk
 
+# Use the open-source board-info file if none was set by the vendor
+# config.
+TARGET_BOARD_INFO_FILE ?= device/samsung/maguro/board-info.txt
+
 # Use the part that is common between all tunas
 include device/samsung/tuna/BoardConfig.mk
+
