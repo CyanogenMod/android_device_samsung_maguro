2833f77f237cf3025ffe22e0642d41bdf12094f5
KalimochoAz <calimochoazucarado@gmail.com>
2012-01-27 10:13:44 +0100

    Add MaguroParts
    Add this to control gamma color hack and other
    new device settings will be there in the future.
    
    patch 2: Moved to tuna
    
    Change-Id: I3d04c08e68e73d1f309ae4f15b54c1cf5c05c511

--- full_maguro.mk
+++ full_maguro.mk
@@ -26,7 +26,8 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 
 # Camera
 PRODUCT_PACKAGES := \
-    Camera
+    Camera \
+    GNexusParts
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
