b11b8584b8eef2f6bf075c1dabd74e8010751fe0
Koushik Dutta <koushd@gmail.com>
2012-02-23 05:13:05 -0800

    explicitly specify it is GSM
    Change-Id: I4d1435642105f9a96f5519d64b9cc15f649cb4fb

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GN
+PRODUCT_RELEASE_NAME := GN-GSM
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
