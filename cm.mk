b497e369b7e2cb6f026a8f3601e31728050bef72
blunden <blunden2@gmail.com>
2012-03-30 03:49:25 +0200

    Maguro: Update BUILD_DESCRIPTION and FINGERPRINT for IMM76D
    Change-Id: I662d793bf3d2ecd86593e8a51a453e1a56bc3ab5

--- cm.mk
+++ cm.mk
@@ -21,4 +21,4 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.2/ICL53F/235179:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.2 ICL53F 235179 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.4/IMM76D/299849:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76D 299849 release-keys"
