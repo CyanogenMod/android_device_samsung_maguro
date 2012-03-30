CyanogenMod/android_device_moto_wingray

Self-extractors for IMM30D
Change-Id: Ia5c334bc6e8c62a7237176440647287559249d60

self-extractors/generate-packages.sh
@@ -22,8 +22,9 @@
 # 238649 = IML74G
 # 239410 = IML74K
 # 257829 = IMM30B
-ZIP=tervigon-ota-257829.zip
-BUILD=imm30b
+# 262866 = IMM30D
+ZIP=tervigon-ota-262866.zip
+BUILD=imm30d
 ROOTDEVICE=stingray
 DEVICE=wingray
 MANUFACTURER=moto

CyanogenMod/android_device_samsung_maguro

Change-Id: If3b0ff3cf4b6e5d4742dfc0ad3d072347e725102

self-extractors/generate-packages.sh
@@ -23,8 +23,9 @@
 # 238649 = IML74G
 # 239410 = IML74K
 # 257829 = IMM30B
-ZIP=yakju-ota-257829.zip
-BUILD=imm30b
+# 262866 = IMM30D
+ZIP=yakju-ota-262866.zip
+BUILD=imm30d
 ROOTDEVICE=maguro
 DEVICE=maguro
 MANUFACTURER=samsung