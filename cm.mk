3148f4c8e7b7b5ffac861845d304a914f8027471
blunden <blunden2@gmail.com>
2012-05-03 18:10:41 +0200

    maguro: Update ro.build.fingerprint and description to match IMM76I OTA
    Change-Id: I5e6edd14fd69731f5d23c4afa8e14ab98aceab20

--- cm.mk
+++ cm.mk
@@ -24,4 +24,4 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.4/IMM76D/299849:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76D 299849 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76I 330937 release-keys"
