210dfae03a2b838aa145bfab4d4786ebcee8be8d
Axel2033 <axel2033@gmail.com>
2012-07-22 20:58:19 +0400

    maguro: Update ro.build.fingerprint and description to match JRO03C OTA
    Change-Id: I5b308a2ed5a867f484424be0570f10a86653f8de

--- cm.mk
+++ cm.mk
@@ -24,4 +24,4 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76I 330937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.1.1/JRO03C/398337:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.1.1 JRO03C 398337 release-keys"
