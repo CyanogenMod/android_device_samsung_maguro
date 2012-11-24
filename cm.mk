f54ecb6aff80ee7f0bbd3f84f2c645f3363ae24b
KalimochoAz <calimochoazucarado@gmail.com>
2012-11-24 01:48:34 +0100

    Update maguro fingerprint
    Change-Id: If6dac8a02bc7a7c0bb72dea0d75e9d89681c3219

--- cm.mk
+++ cm.mk
@@ -25,4 +25,4 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.1.2/JZO54K/485486:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.1.2 JZO54K 485486 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=takju BUILD_FINGERPRINT="google/takju/maguro:4.2/JOP40C/517167:user/release-keys" PRIVATE_BUILD_DESC="takju-user 4.2 JOP40C 517167 release-keys"
