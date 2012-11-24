763ef765e697b0f47eab65fb07d65ede934def8a
lithid <long.jeremie@gmail.com>
2012-11-24 10:13:47 -0500

    Update extract-files and proprietary-blobs
    Patchset 2: Revert to uniform style, commited local style.

--- extract-files.sh
+++ extract-files.sh
@@ -23,6 +23,7 @@ MANUFACTURER=samsung
 mkdir -p ../../../vendor/$MANUFACTURER/$DEVICE/proprietary
 adb pull /system/bin/fRom ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/fRom
 chmod 755 ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/fRom
+adb pull system/lib/libdrmdecrypt.so ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/libdrmdecrypt.so
 adb pull /system/lib/libsecril-client.so ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/libsecril-client.so
 adb pull /system/vendor/bin/pvrsrvctl ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/pvrsrvctl
 chmod 755 ../../../vendor/$MANUFACTURER/$DEVICE/proprietary/pvrsrvctl
@@ -70,6 +71,7 @@ PRODUCT_COPY_FILES := \\
 # All the blobs necessary for maguro
 PRODUCT_COPY_FILES += \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/fRom:system/bin/fRom \\
+    vendor/__MANUFACTURER__/__DEVICE__/proprietary/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/libsecril-client.so:system/lib/libsecril-client.so \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/pvrsrvctl:system/vendor/bin/pvrsrvctl \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/sirfgps.conf:system/vendor/etc/sirfgps.conf \\
