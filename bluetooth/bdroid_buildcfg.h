b40835e5ba7c62565499a87ee0e2b8663e2fb1b2
Matthew Xie <mattx@google.com>
2013-05-08 19:06:32 -0700

    Turn off BLE, GATT, and SMP of Bluetooth stack
    bug 8664724
    
    Change-Id: I3ef91349cce5bcd0323690e9be0835e07dbd9f28

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy Nexus 2"
+#define BTM_DEF_LOCAL_NAME   "Galaxy Nexus"
 
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 #endif
