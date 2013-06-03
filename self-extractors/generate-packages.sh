cdeb6b6c1e283ddcba41219e0d357ec3a373dc26
Jean-Baptiste Queru <jbq@google.com>
2013-06-03 07:56:22 -0700

    Self-extractors for JWR64
    Change-Id: I18dc13e3870e93a1b882f0f803cf5890ff234bf0

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -57,6 +57,7 @@
 # 689345 = JWR58
 # 690834 = JWR59
 # 692263 = JWR60
+# 695489 = JWR64
 # end jb-mr2-dev
 BRANCH=jb-mr2-dev
 if test $BRANCH = ics-mr1
@@ -81,8 +82,8 @@ then
 fi # jb-mr1.1-dev
 if test $BRANCH = jb-mr2-dev
 then
-  ZIP=yakju-ota-692263
-  BUILD=jwr60
+  ZIP=yakju-ota-695489
+  BUILD=jwr64
 fi # jb-mr2-dev
 ROOTDEVICE=maguro
 DEVICE=maguro
