004aaa77f3c394bdb0644cd49620418f33699510
djMesias <djmesias@yahoo.es>
2013-07-29 12:42:44 +0200

    maguro: Missing 'android-4.3_r2.1' changes
    Change-Id: I69bebc3ab25e1be2c66ad423a645b967963d2a50

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -14,25 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# start ics
 # 223971 = ITL41D
 # 228551 = ITL41F
-# 236517 = IML70C
-# 237179 = IML73
-# 237867 = IML74B
-# 238432 = IML74E
-# 238649 = IML74G
-# 239410 = IML74K
-# 257829 = IMM30B
-# 262866 = IMM30D
+# 235179 = ICL53F
 # 299849 = IMM76D
 # 330937 = IMM76I
-# end ics
-# start jellybean
-# 241580 = IRL89
-# 241968 = IRM03
-# 398337 = JRO03C
-# end jellybean
 # 397360 = JRO02C
 # 398337 = JRO03C
 # 477516 = JZO54I
