2f260701d47606e93d1d528fbf877ccf908d4fad
djMesias <djmesias@yahoo.es>
2013-07-29 12:42:44 +0200

    maguro: Missing 'android-4.3_r2.1' changes
     -Update to JWR66V
    
    Change-Id: I69bebc3ab25e1be2c66ad423a645b967963d2a50

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,25 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# start ics
 # 223971 = ITL41D
 # 228551 = ITL41F
-# 236517 = IML70C
-# 237179 = IML73
-# 237867 = IML74B
-# 238432 = IML74E
-# 238649 = IML74G
-# 239410 = IML74K
-# 257829 = IMM30B
-# 262866 = IMM30D
+# 235179 = ICL53F
 # 299849 = IMM76D
 # 330937 = IMM76I
-# end ics
-# start jellybean
-# 241580 = IRL89
-# 241968 = IRM03
-# 398337 = JRO03C
-# end jellybean
 # 397360 = JRO02C
 # 398337 = JRO03C
 # 477516 = JZO54I
@@ -43,23 +29,29 @@
 # 533553 = JOP40D
 # 551245 = JDP82
 # 573038 = JDQ39
+# 683083 = JWR51
+# 689345 = JWR58
+# 690834 = JWR59
+# 704243 = JWR66G
+# 711294 = JWR66N
+# 737497 = JWR66V
 
 source ../../../common/clear-factory-images-variables.sh
-BUILD=573038
+BUILD=737497
 DEVICE=maguro
 PRODUCT=yakju
-VERSION=jdq39
+VERSION=jwr66v
 SRCPREFIX=signed-
-BOOTLOADER=primelc03
+BOOTLOADER=primemd04
 RADIO=i9250xxlj1
 source ../../../common/generate-factory-images-common.sh
 
 source ../../../common/clear-factory-images-variables.sh
-BUILD=573038
+BUILD=737497
 DEVICE=maguro
 PRODUCT=takju
-VERSION=jdq39
+VERSION=jwr66v
 SRCPREFIX=signed-
-BOOTLOADER=primelc03
+BOOTLOADER=primemd04
 RADIO=i9250xxlj1
 source ../../../common/generate-factory-images-common.sh
