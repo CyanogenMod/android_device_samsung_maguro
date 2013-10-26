15c9be2e71549c40531eeddea5e050e3307e4650
Lorenzo M <djmesias@yahoo.es>
2013-10-26 10:02:40 +0200

    maguro: remove Gallery2 locally
    Don't worry. We'll still have our Gallery&Camera (build package).
    
    Change-Id: I66ad96a498d079424ad43ba3b8cbe0448416d8c8

--- full_maguro.mk
+++ full_maguro.mk
@@ -24,10 +24,6 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Camera
-PRODUCT_PACKAGES := \
-    Gallery2
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
