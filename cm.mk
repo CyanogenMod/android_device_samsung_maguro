2e46ccba39b8a03ea035ecae547ddb6cc0be3c2a
Michael Limb <something15525@gmail.com>
2013-10-29 01:06:46 -0700

    maguro: inherit from AOSP .mk file
    Inherit from aosp_maguro.mk file in cm.mk, since that is the AOSP
    standard now.
    
    Change-Id: I3ce58d1995145b8a79784ce8b3eda5da7a3cef68

--- cm.mk
+++ cm.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+$(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
