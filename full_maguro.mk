a715849b5a901a6e40f8371da691ca72c24619d9
Michael Limb <something15525@gmail.com>
2013-10-29 01:05:52 -0700

    maguro: Switch to World APNs list
    Remove gsm.mk and don't copy sample APNs file, use the World APNs
    list.
    
    Change-Id: I6eec76ea6c9b61ee47d243ce2d9e4abdca34d84c

--- full_maguro.mk
+++ full_maguro.mk
@@ -22,7 +22,7 @@
 #
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+# PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
