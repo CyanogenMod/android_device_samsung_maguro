786fe38b7baf58c0851d6ed36b083fda552802f3
Lorenzo M <djmesias@yahoo.es>
2013-12-14 08:58:59 +0100

    maguro: update fingerprint to JWR66Y
    - Lastest official build fingerprint
    
    Change-Id: I262c83b9b563de02c7ae24e2a722d299bf67dd76

--- cm.mk
+++ cm.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66V/737497:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66V 737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
