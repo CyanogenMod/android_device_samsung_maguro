af459b8ab433eb9cbd000a047ec77a0e6c3ff8c7
Ziyann <jaraidaniel@gmail.com>
2014-11-14 08:21:53 +0100

    maguro: don't copy sample APNs
    Change-Id: Ie4fb64729d160213bfeaff580a54730540b0a1e7

--- full_maguro.mk
+++ full_maguro.mk
@@ -21,9 +21,6 @@
 # lines, full and maguro, hence its name.
 #
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
