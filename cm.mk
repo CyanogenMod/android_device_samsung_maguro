02093df4fed2ecba975c459bc16f244a70b9a005
Ziyann <jaraidaniel@gmail.com>
2014-11-14 08:03:33 +0100

    maguro: add cm.mk
    Change-Id: I414d09d7cf8eb9aa180ab5fd23729f88ba2df59c

---
+++ cm.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := GN-GSM
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := maguro
+PRODUCT_NAME := cm_maguro
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
