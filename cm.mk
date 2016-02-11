2677d9bf48a15edd173639a2bd37a7f91a58f636
Ziyan <jaraidaniel@gmail.com>
2016-02-11 01:31:53 +0100

    maguro: switch to common_mini_phone config to save space
    Change-Id: If0ab26ab6852095d05c65d71b4b1219b10ee8a58

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
